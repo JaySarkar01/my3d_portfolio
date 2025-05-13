63447b5b851c86716d1719a3ee982c7b1512c8b4
JaySarkar01 <jaysarkari9@gmail.com>
2025-05-13 20:16:30 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function App() {
+
+  return (
+    <div>
+      <h3 className='flex text-3xl font-bold underline text-center mt-10'>
+        Hello Vite + React + Tailwind CSS! 
+      </h3>
+    </div>
+  )
+}
+
+export default App
